abd363d004ba2bb0ba6a8b5fa921bc25e5377a15
Begum <topcuoglu.begum@gmail.com>
2019-12-09 11:47:50 -0500

    output

--- code/bash/mothurOptiFit.sh
+++ code/bash/mothurOptiFit.sh
@@ -5,9 +5,9 @@
 # University of Michigan
 
 
-export WORKDIR=data/process
+export WORKDIR=data/process/
 export NUM=2003650
 
 # We are now ready to use OptiFit to fit left out sample to the rest.
 
-mothur "#cluster.fit(fasta="${WORKDIR}"/sample."${NUM}".fasta, column="${WORKDIR}"/sample."${NUM}".dist, count="${WORKDIR}"/sample."${NUM}".count_table, reffasta="${WORKDIR}"/without."${NUM}".fasta, refcolumn="${WORKDIR}"/without."${NUM}".dist, reflist="${WORKDIR}"/without.opti_mcc."${NUM}".list, method=closed)"
+mothur "#cluster.fit(fasta="${WORKDIR}"/sample."${NUM}".fasta, column="${WORKDIR}"/sample."${NUM}".dist, count="${WORKDIR}"/sample."${NUM}".dist, reffasta="${WORKDIR}"/without."${NUM}".fasta, refcolumn="${WORKDIR}"/without."${NUM}".dist, reflist="${WORKDIR}"/without.opti_mcc."${NUM}".list, method=closed)"
