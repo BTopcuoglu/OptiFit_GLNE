4d0c6707938bbfd2e887c11d9d61fbfe3996dcd6
Begum <topcuoglu.begum@gmail.com>
2019-12-10 13:49:52 -0500

    update

--- code/bash/getFiles.sh
+++ code/bash/getFiles.sh
@@ -1,19 +1,11 @@
-#! /bin/bash
-# getFiles.sh
-# Begum Topcuoglu
-# Schloss Lab
-# University of Michigan
-
-##################
-# Set Script Env #
-##################
+#!/bin/bash
 
 # Load packages
 module load Bioinformatics
-module load sratoolkit/2.7.0
+module load sratoolkit/2.9.6
 
 # Other variables
-export WORKDIR=data/mothur/raw/baxter
+export DATA=data/mothur/raw/baxter
 export OUTDIR=data/process/baxter
 
 
@@ -29,19 +21,37 @@ mkdir -p "${OUTDIR}"
 
 curl -o "${OUTDIR}"/glne007.files https://raw.githubusercontent.com/SchlossLab/Baxter_glne007Modeling_GenomeMed_2015/master/data/glne007.files
 
+curl -o "${DATA}"/sra_info.tsv https://github.com/SchlossLab/Schloss_Singletons_XXXXX_2019/blob/master/data/human/sra_info.tsv
+
 ##########################
 # Extract sra files #
 ##########################
 
-bash code/bash/datasets_download.sh
+# The input should be something like data/marine. This script assumes that there is a file called
+# $DATA/sra_info.tsv, which is generated from hitting the "RunInfo Table" button that can be found
+# on pages like https://trace.ncbi.nlm.nih.gov/Traces/study/?acc=ERP012016 (replace ERP012016 with
+# the SRA Study identifier. The output will be a directory ($DATA) full of paired fastq.gz files
+# for downstream processing
+
+# Start by cleanining up directory by removing pre-existing fastq files
+rm -f "${DATA}"/*fastq*
 
 
+# Need to get the column that contains the run file names that start wtih SRR (SRA) or ERR (ENA). The
+# column position is not consistent across datasets, so need to remove column header and use sed to
+# isolate column
+SRRS=`tail -n +2 "${DATA}"/sra_info.tsv | sed -E "s/.*(.RR\S*)\t.*/\1/"`
 
-# Convert to fasta files that will be used
-#for sample in data/mothur/raw/baxter/*.sra
-#do
-	#fastq-dump --split-files $sample -O "${OUTDIR}"
-#done
+
+# Loop through each run file and pull it down from the SRA. After downloaded, we want to split it into
+# the R1 and R2 files. Finaly, we'll compress the files with gzip
+for sample in $SRRS
+do
+  echo $sample
+	prefetch $sample
+    fastq-dump --split-files $sample -O "${OUTDIR}"
+done
+
 
 # Rename files file to reflect where fastq files are
-#sed -ie 's/SRR/data\/process\/baxter\/SRR/g' data/process/baxter/glne007.files
+sed -ie 's/SRR/data\/process\/baxter\/SRR/g' data/process/baxter/glne007.files
