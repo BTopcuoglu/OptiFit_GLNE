0b2ec0147da0ee258da0ad25151143d0690d33a6
William L. Close <close.will@gmail.com>
2019-12-18 17:12:12 -0500

    fixing variable expansion so if statement works properly

--- code/bash/getSRAFiles.sh
+++ code/bash/getSRAFiles.sh
@@ -29,7 +29,7 @@ mkdir -p "${OUTDIR}"
 # The output will be a directory ($OUTDIR) full of paired fastq.gz files for downstream processing
 
 
-# Need to get the column that contains the run file names that start wtih SRR (SRA) or ERR (ENA). The
+# Need to get the column that contains the run file names that start wtih SRR (SRA). The
 # column position is not consistent across datasets, so need to remove column header and use sed to
 # isolate column
 SRRS=$(tail -n +2 "${SRAINFO}" | sed 's:.*\(SRR[0-9]*\).*:\1:')
@@ -37,11 +37,10 @@ SRRS=$(tail -n +2 "${SRAINFO}" | sed 's:.*\(SRR[0-9]*\).*:\1:')
 
 # Loop through each run file and pull it down from the SRA. After downloaded, we want to split it into
 # the R1 and R2 files. Finally, we'll compress the files with gzip
-for sample in $SRRS
+for SAMPLE in $(echo "${SRRS}")
 do
-	echo $sample
-	prefetch $sample
-    fastq-dump --split-files -O "${OUTDIR}" --gzip $sample
+	prefetch "${SAMPLE}"
+    fastq-dump --split-files -O "${OUTDIR}" --gzip "${SAMPLE}"
 done
 
 
@@ -50,10 +49,10 @@ done
 SINGLE_FILES=$(find "${OUTDIR}" -name "*fastq.gz" | cut -f 1 -d _ | sort | uniq -u)
 
 # If $SINGLE_FILES is set (not empty or ""), remove those files
-if [ -n $SINGLE_FILES ]
+if [ -n "${SINGLE_FILES}" ]
 then
-	rm $SINGLE_FILES
+	rm "${SINGLE_FILES}"
 fi
 
-# Cleaning up SRA temp directories
+# Cleaning up SRA temp directories (puts them in current working directory)
 find ./ -type d -maxdepth 1 -regex ".*/SRR.*" -exec rm -r {} \;
