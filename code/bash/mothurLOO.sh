169587386d0a08b85f92a23dfb4b6b2d89ab337e
William L. Close <close.will@gmail.com>
2019-12-19 15:35:55 -0500

    removing use of tmp dir, removing shared file generation, and cleaning up

--- code/bash/mothurLOO.sh
+++ code/bash/mothurLOO.sh
@@ -10,37 +10,29 @@
 ##################
 
 # Set the variables to be used in this script
-# export GROUPS=${1:?ERROR: Need to define GROUPS.}
 export FASTA=${1:?ERROR: Need to define FASTA.}
 export COUNT=${2:?ERROR: Need to define COUNT.}
 export TAXONOMY=${3:?ERROR: Need to define TAXONOMY.}
 export SAMPLE=${4:?ERROR: Need to define SAMPLE.}
 
 # Other variables
-export OUTDIR=data/process/loo/
-export TMP="${OUTDIR}"/"${SAMPLE}"/ # Tmp dir based on sample name to keep things separate during parallelization
-
-# export NUM=2003650
-# export GROUP=data/process/baxter/final/full.groups
-# export FASTA=data/process/baxter/final/full.fasta
-# export COUNT=data/process/baxter/final/full.count_table
-# export TAXONOMY=data/process/baxter/final/full.taxonomy
+export OUTDIR=data/process/loo/"${SAMPLE}"/ # Output dir based on sample name to keep things separate during parallelization/organized
 
 
 
-###############################################
-# Generate Individual Shared File for Sample  #
-###############################################
+###################################################
+# Generate Sequence Distances for Sample Left Out #
+###################################################
 
 # Make output dirs if they don't exist
-mkdir -p "${TMP}"/
+mkdir -p "${OUTDIR}"/
 
 # Create cluster distance file for individual sample
-mothur "#get.groups(fasta="${FASTA}", count="${COUNT}", taxonomy="${TAXONOMY}",  groups="${SAMPLE}", outputdir="${TMP}"/);
+mothur "#get.groups(fasta="${FASTA}", count="${COUNT}", taxonomy="${TAXONOMY}",  groups="${SAMPLE}", outputdir="${OUTDIR}"/);
 	dist.seqs(fasta=current, cutoff=0.03)"
 
 # Renaming outputs of files generated from single sample
-for FILE in $(find "${TMP}"/ -regex ".*precluster.*"); do
+for FILE in $(find "${OUTDIR}"/ -regex ".*precluster.*"); do
 
 	# Uses path and suffix of input file to rename output file using $SAMPLE and 'in' to represent the file is for
 	# the individual sample only.
@@ -53,18 +45,17 @@ done
 
 
 
-########################################################
-# Generate shared file for all samples but the one #
-########################################################
+########################################
+# Generate Clusters After Leave One Out #
+########################################
 
 # Cluster all sequences while leaving out the specified sample
-mothur "#remove.groups(fasta="${FASTA}", count="${COUNT}", taxonomy="${TAXONOMY}",  groups="${SAMPLE}", outputdir="${TMP}"/);
+mothur "#remove.groups(fasta="${FASTA}", count="${COUNT}", taxonomy="${TAXONOMY}",  groups="${SAMPLE}", outputdir="${OUTDIR}"/);
 	dist.seqs(fasta=current, cutoff=0.03);
-	cluster(column=current, count=current);
-	make.shared(list=current, count=current, label=0.03)"
+	cluster(column=current, count=current)"
 
 # Renaming outputs of files generated after leaving the specified sample out
-for FILE in $(find "${TMP}"/ -regex ".*precluster.*"); do
+for FILE in $(find "${OUTDIR}"/ -regex ".*precluster.*"); do
 
 	# Uses path and suffix of input file to rename output file using $SAMPLE and 'out' to represent the file is for
 	# all of the other samples after the specified sample has been left out.
