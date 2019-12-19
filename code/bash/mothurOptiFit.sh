c7cb4e0f13aa6fab5749522b03a28240b4bf2a63
William L. Close <close.will@gmail.com>
2019-12-19 17:14:26 -0500

    outputting files to separate dir for each sample and renaming slightly

--- code/bash/mothurOptiFit.sh
+++ code/bash/mothurOptiFit.sh
@@ -26,6 +26,26 @@ export OUTDIR=data/process/optifit/
 # Clustering Leave-One-Out Outputs Using OptiFit #
 ##################################################
 
+# Pulling sample name from file name
+SAMPLE=$(echo "${INFASTA}" | sed "s:.*/\([0-9]*\).in.fasta:\1:")
+
+# Creating subdirectory using sample name for storing output files
+SUBDIR="${OUTDIR}"/"${SAMPLE}"/
+
+# Create subdirectory
+mkdir -p "${SUBDIR}"/
+
 # Running OptiFit to cluster left out sample with reference clusters
-mothur "#cluster.fit(fasta="${INFASTA}", column="${INDIST}", count="${INCOUNT}", reffasta="${OUTFASTA}", refcolumn="${OUTDIST}", reflist="${OUTLIST}", method=closed, outputdir="${OUTDIR}");
+mothur "#cluster.fit(fasta="${INFASTA}", column="${INDIST}", count="${INCOUNT}", reffasta="${OUTFASTA}", refcolumn="${OUTDIST}", reflist="${OUTLIST}", method=closed, outputdir="${SUBDIR}");
 	make.shared(list=current, count=current, label=0.03)"
+
+# Cleaning up names
+for FILE in $(find "${SUBDIR}"/ -type f); do
+
+	# Removing extra name information
+	REPLACEMENT=$(echo "${FILE}" | sed "s:in\.::")
+
+	# Rename files using new format
+	mv "${FILE}" "${REPLACEMENT}"
+
+done
