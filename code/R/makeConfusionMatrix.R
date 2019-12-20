d37fba2a3e301593f297cf80d56ccdf456f58f1d
William L. Close <close.will@gmail.com>
2019-12-20 17:49:54 -0500

    finishing putting together results script

--- code/R/makeConfusionMatrix.R
+++ code/R/makeConfusionMatrix.R
@@ -5,7 +5,7 @@
 # University of Michigan
 
 # Purpose: Combines output from L2 logistic regression ML pipeline into a confusion matrix.
-# Usage: Rscript makeConfusionMatrix.R CVFILE1 CVFILE2 CVFILE3 ... CVFILEN PREDFILE1 PREDFILE2 PREDFILE3 ... PREDFILEN
+# Usage: Rscript makeConfusionMatrix.R CVFILE1 CVFILE2 CVFILE3 ... CVFILEN PREDFILE1 PREDFILE2 PREDFILE3 ... PREDFILEN DXDIFFTHRESH CLASSTHRESH
 
 # Setting environment -----------------------------------------------------
 
@@ -13,10 +13,13 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 # Variables defined by user
-mlFiles <- args # SRA run metadata
+metadata <- args[1] # Sample metadata with diagnosis information
+mlFiles <- sort(args[2:length(args)-2]) # Output files from ML pipeline (cvAUC and prediction scores)
+dxDiffThresh <- args[length(args)-1] # Threshold for wanting to investigate health data because prediction scores are too close
+classThresh <- args[length(args)] # Threshold for calling normal based on prediction values
 
 # Other variables
-outDir <- "results/tables/"
+outDir <- "data/learning/results/"
 
 
 
@@ -31,53 +34,64 @@ library(tidyverse)
 # Creating output directory if it doesn't already exist
 dir.create(outDir, recursive = TRUE, showWarnings=FALSE)
 
-mlFiles <- sort(list.files("data/learning/", full.names = T))
-metadata <- "data/test_process/metadata.tsv"
-dxDiffThresh <- 0.05 # Threshold for wanting to investigate health data because prediction scores are too close
-classThresh <- 0.5 # Threshold for calling normal based on prediction values
 
 
-# Importing metadata and recoding to either be normal = 0 or cancer = 1
-meta <- read_tsv(metadata, col_types = cols()) %>% 
-  mutate(sample = as.character(sample),
-         meta_class = case_when(Dx_Bin == "Adenoma" ~ 0, # Recoding diagnoses
-                        Dx_Bin == "Normal" ~ 0,
-                        Dx_Bin == "High Risk Normal" ~ 0,
-                        Dx_Bin == "adv Adenoma" ~ 1,
-                        Dx_Bin == "Cancer" ~ 1)) %>% 
-  select(sample, meta_class)
-
+# Collating results -------------------------------------------------------
 
 # Extracting sample names from file names
 sample_names <- str_extract(mlFiles, "\\d+") %>% 
   unique()
 
-# Subsetting out cross validated AUC score files
-cv_files <- str_subset(mlFiles, "cv")
-
-# Subsetting out prediction score files
-prediction_files <- str_subset(mlFiles, "prediction")
-
-
-
 # Reading in cvAUC files and adding col for sample name
-cv <- map_df(cv_files, read_csv, col_types = cols()) %>% 
+cv <- str_subset(mlFiles, "cv") %>% 
+  map_df(read_csv, col_types = cols()) %>% 
   add_column(sample = sample_names, .before = "cv_auc")
 
 # Reading in prediction files and adding col for sample name
-prediction <- map_df(prediction_files, read_csv, col_types = cols()) %>% 
+prediction <- str_subset(mlFiles, "prediction") %>% 
+  map_df(prediction_files, read_csv, col_types = cols()) %>% 
   add_column(sample = sample_names, .before = "cancer")
 
+# Importing metadata and recoding to either be normal = 0 or cancer = 1
+meta <- read_tsv(metadata, col_types = cols()) %>% 
+  mutate(sample = as.character(sample),
+         meta_class = case_when(Dx_Bin == "Adenoma" ~ 0, # Recoding diagnoses
+                                Dx_Bin == "Normal" ~ 0,
+                                Dx_Bin == "High Risk Normal" ~ 0,
+                                Dx_Bin == "adv Adenoma" ~ 1,
+                                Dx_Bin == "Cancer" ~ 1)) %>% 
+  select(sample, meta_class) # Removing unnecessary cols
 
-
-
-# Creating classification df
-classification <- left_join(cv, prediction, by = "sample") %>% 
+# Creating results df
+results <- left_join(cv, prediction, by = "sample") %>% 
   mutate(dx_diff = abs(cancer - normal), # Difference between prediction scores for dx
          dx_flag = case_when(dx_diff <= 0.05 ~ 1, # Noting whether classifications need to verified by searching 
                           dx_diff > 0.05 ~ 0),
          pred_class = case_when(normal >= classThresh ~ 0, # Determining classification using threshold of classThresh
                                     normal < classThresh ~ 1)) %>% 
-  left_join(meta, by = "sample")
+  left_join(meta, by = "sample") # Adding metadata classification
+
+# Writing out collated results
+write_tsv(results, path = paste0(outDir, "model_results.tsv"))
 
 
+
+# Determining confusion matrix --------------------------------------------
+
+# Different categories for confusion matrix classification
+# TN = true neg, TP = true pos, FN = false neg, FP = false pos
+conf_labels <- tibble(conf = c("TN", "TP", "FN", "FP"))
+
+# Calculating confusion matrix groups
+conf <- results %>% 
+  mutate(conf = case_when(meta_class == 0 & pred_class == 0 ~ "TN",
+                          meta_class == 1 & pred_class == 1 ~ "TP",
+                          meta_class > pred_class ~ "FN",
+                          meta_class < pred_class ~ "FP")) %>% 
+  group_by(conf) %>% 
+  count() %>% # Adding up different categories
+  right_join(conf_labels, by = "conf") %>% # Filling in any missing confusion matrix categories
+  replace_na(list(n = 0)) # Replacing missing values with 0
+
+# Writing out confusion matrix results
+write_tsv(conf, path = paste0(outDir, "confusion_matrix.tsv"))
