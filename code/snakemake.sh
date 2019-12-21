69aaeb69b8355058cfb247dd22aa2852f26ec522
William L. Close <close.will@gmail.com>
2019-12-20 22:35:44 -0500

    updating slurm cluster script

--- code/snakemake.sh
+++ code/snakemake.sh
@@ -7,30 +7,31 @@
 ###############################
 
 # Name
-#SBATCH --job-name=mothurContigs.sh
+#SBATCH --job-name=snakemake
 
 # Resources
 # For MPI, increase ntasks-per-node
 # For multithreading, increase cpus-per-task
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=12
-#SBATCH --mem-per-cpu=8000mb
-#SBATCH --time=20:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4GB
+#SBATCH --time=72:00:00
 
 # Account
-#SBATCH --account=pschloss
+#SBATCH --account=ACCOUNT
 #SBATCH --partition=standard
 
 # Logs
-#SBATCH --mail-user=begumtop@umich.edu
+#SBATCH --mail-user=EMAIL
 #SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=%x-%j.out
 
 # Environment
-##SBATCH --export=ALL
+#SBATCH --export=ALL
 
-# Load modules R and Rtidyverse
-
+# Connecting node to internet
+source /etc/profile.d/http_proxy.sh 
 
 # List compute nodes allocated to the job
 if [[ $SLURM_JOB_NODELIST ]] ; then
@@ -39,19 +40,16 @@ if [[ $SLURM_JOB_NODELIST ]] ; then
 	echo -e "\n"
 fi
 
-mkdir data/temp
+
 
 #####################
 #                   #
 #  2) Job Commands  #
 #                   #
 #####################
-bash code/bash/getFiles.sh
 
-bash code/bash/mothurReferences.sh
+# Making output dir for snakemake cluster logs
+mkdir -p logs/slurm/
 
-bash code/bash/mothurContigs.sh
-
-bash code/bash/mothurLOO.sh
-
-bash code/bash/mothurOptiFit.sh
+# Initiating snakemake and running workflow in cluster mode
+snakemake --use-conda --verbose --profile config/slurm/  --latency-wait 90
