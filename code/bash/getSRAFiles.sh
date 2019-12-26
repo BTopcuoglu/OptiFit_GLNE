8b9adefd1f26bb473eb80451e2e479f21e9f8baf
William L. Close <close.will@gmail.com>
2019-12-26 18:37:31 -0500

    cleaning up script now that it works

--- code/bash/getSRAFiles.sh
+++ code/bash/getSRAFiles.sh
@@ -13,7 +13,7 @@
 export SEQUENCENAME=${1:?ERROR: Need to define SEQUENCENAME.}
 
 # Other variables
-export OUTDIR=data/raw/test/
+export OUTDIR=data/raw/
 
 
 
@@ -24,40 +24,9 @@ export OUTDIR=data/raw/test/
 # Making output dir
 mkdir -p "${OUTDIR}"
 
-# This script takes a file called SraRunTable.txt, which is generated from hitting the "RunInfo Table"
-# button that can be found on the SRA page https://trace.ncbi.nlm.nih.gov/Traces/study/?acc=SRP062005.
-# The output will be a directory ($OUTDIR) full of paired fastq.gz files for downstream processing
-
-
-# Need to get the column that contains the run file names that start wtih SRR (SRA). The
-# column position is not consistent across datasets, so need to remove column header and use sed to
-# isolate column
-# SRRS=$(tail -n +2 "${SRAINFO}" | sed 's:.*\(SRR[0-9]*\).*:\1:')
-
-
-# Loop through each run file and pull it down from the SRA. After downloaded, we want to split it into
-# the R1 and R2 files. Finally, we'll compress the files with gzip
-# for SAMPLE in $(echo "${SRRS}")
-# do
-# 	prefetch "${SAMPLE}"
-#     fastq-dump --split-files -O "${OUTDIR}" --gzip "${SAMPLE}"
-# done
-
-
+# Downloading named SRR sequence pair
 prefetch "${SEQUENCENAME}"
 fastq-dump --split-files -O "${OUTDIR}" --gzip "${SEQUENCENAME}"
 
-
-# Some SRR files only contain data for one sequence read. So there aren't problems down the road, we
-# want to  make sure all files hav both reads, remove those with only one read
-SINGLE_FILES=$(find "${OUTDIR}" -name "${SEQUENCENAME}*" | cut -f 1 -d _ | sort | uniq -u)
-
-# If $SINGLE_FILES is set (not empty or ""), remove those files
-if [ -n "${SINGLE_FILES}" ]
-then
-	rm "${SINGLE_FILES}"
-fi
-
 # Cleaning up SRA temp directories (puts them in current working directory)
-# find ./ -type d -maxdepth 1 -regex ".*/SRR.*" -exec rm -r {} \;
 find ./ -maxdepth 1 -type d -regex ".*${SEQUENCENAME}" -exec rm -r {} \;
