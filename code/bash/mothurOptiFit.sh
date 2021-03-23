ca9316a0318787e50a688418839410c8197e16f4
courtneyarmour <armourc@med.umich.edu>
2021-03-23 11:08:10 -0400

    add dist to clusterOptiFit

--- code/bash/mothurOptiFit.sh
+++ code/bash/mothurOptiFit.sh
@@ -13,10 +13,11 @@
 FASTA=${1:?ERROR: Need to define FASTA.} # Preclustered fasta file
 COUNT=${2:?ERROR: Need to define COUNT.} # Preclustered count file
 TAXONOMY=${3:?ERROR: Need to define TAXONOMY.} # Preclustered tax file
-SAMPLE=${4:?ERROR: Need to define SAMPLE.} # Sample to be fit to reference files
-REFFASTA=${5:?ERROR: Need to define OUTFASTA.} # Reference fasta file with sample removed
-REFDIST=${6:?ERROR: Need to define OUTDIST.} # Reference dist file with sample removed
-REFLIST=${7:?ERROR: Need to define OUTLIST.} # Reference list file with sample removed
+DIST=${4:?ERROR: Need to define DIST.} # Preclustered distance file
+SAMPLE=${5:?ERROR: Need to define SAMPLE.} # Sample to be fit to reference files
+REFFASTA=${6:?ERROR: Need to define OUTFASTA.} # Reference fasta file with sample removed
+REFDIST=${7:?ERROR: Need to define OUTDIST.} # Reference dist file with sample removed
+REFLIST=${8:?ERROR: Need to define OUTLIST.} # Reference list file with sample removed
 
 
 # Other variables
@@ -40,9 +41,9 @@ fi
 
 # Selecting sample to be fit, subsampling input files to account for 'closed' clustering method, and fitting sample to previous shared file
 mothur "#set.current(outputdir="${OUTDIR}"/, processors="${NPROC}");
-	get.groups(fasta="${FASTA}", count="${COUNT}", taxonomy="${TAXONOMY}",  groups="${SAMPLE}");
+	get.groups(fasta="${FASTA}", count="${COUNT}", taxonomy="${TAXONOMY}",  groups="${SAMPLE}", column="${DIST}");
 	sub.sample(fasta=current, count=current, taxonomy=current, size="${SUBSIZE}");
-	rename.seqs(fasta=current,count=current); 
+	rename.seqs(fasta=current,count=current);
 	cluster.fit(fasta=current, column=current, count=current, reffasta="${REFFASTA}", refcolumn="${REFDIST}", reflist="${REFLIST}", method=closed);
 	make.shared(list=current, count=current, label=0.03);
 	list.seqs(list=current)"  # creates accnos file of seqs in OTUs
