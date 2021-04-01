ca67e29a9ff8671aa307d237de438a754df02db3
Courtney Armour <courtneyrarmour@gmail.com>
2021-04-01 17:12:39 -0400

    update mothurPrecluster file check

--- code/bash/mothurPrecluster.sh
+++ code/bash/mothurPrecluster.sh
@@ -36,20 +36,20 @@ if [ -n "$(ls -A "${OUTDIR}")" ]; then
 	rm "${OUTDIR}"/*
 fi
 
-#mothur "#make.contigs(file="${FILESFILE}", outputdir="${TMP}"/, processors="${NPROC}");
-#	screen.seqs(fasta=current, group=current, maxambig=0, maxlength=275, maxhomop=8);
-#	unique.seqs(fasta=current);
-#	count.seqs(name=current, group=current);
-#	align.seqs(fasta=current, reference="${SILVAV4}");
-#	screen.seqs(fasta=current, count=current, start=1968, end=11550);
-#	filter.seqs(fasta=current, vertical=T, trump=.);
-#	unique.seqs(fasta=current, count=current);
-#	pre.cluster(fasta=current, count=current, diffs=2);
-#	chimera.vsearch(fasta=current, count=current, dereplicate=T);
-#	remove.seqs(fasta=current, accnos=current);
-#	classify.seqs(fasta=current, count=current, reference="${RDPFASTA}", taxonomy="${RDPTAX}", cutoff=80);
-#	remove.lineage(fasta=current, count=current, taxonomy=current, taxon=Chloroplast-Mitochondria-unknown-Archaea-Eukaryota);
-#	dist.seqs(fasta=current,cutoff=0.03)"
+mothur "#make.contigs(file="${FILESFILE}", outputdir="${TMP}"/, processors="${NPROC}");
+	screen.seqs(fasta=current, group=current, maxambig=0, maxlength=275, maxhomop=8);
+	unique.seqs(fasta=current);
+	count.seqs(name=current, group=current);
+	align.seqs(fasta=current, reference="${SILVAV4}");
+	screen.seqs(fasta=current, count=current, start=1968, end=11550);
+	filter.seqs(fasta=current, vertical=T, trump=.);
+	unique.seqs(fasta=current, count=current);
+	pre.cluster(fasta=current, count=current, diffs=2);
+	chimera.vsearch(fasta=current, count=current, dereplicate=T);
+	remove.seqs(fasta=current, accnos=current);
+	classify.seqs(fasta=current, count=current, reference="${RDPFASTA}", taxonomy="${RDPTAX}", cutoff=80);
+	remove.lineage(fasta=current, count=current, taxonomy=current, taxon=Chloroplast-Mitochondria-unknown-Archaea-Eukaryota);
+	dist.seqs(fasta=current,cutoff=0.03)"
 
 
 # Moving and renaming important files for downstream use
@@ -58,6 +58,7 @@ fi
 #mv "${TMP}"/*.precluster.pick.pds.wang.pick.taxonomy "${OUTDIR}"/glne.precluster.taxonomy
 #mv "${TMP}"/*.precluster.pick.pick.dist "${OUTDIR}"/glne.precluster.dist
 
+#new filenames with 1.45
 mv "${TMP}"/*.precluster.pick.fasta "${OUTDIR}"/glne.precluster.fasta
 mv "${TMP}"/*.precluster.pick.count_table "${OUTDIR}"/glne.precluster.count_table
 mv "${TMP}"/*.precluster.pds.wang.taxonomy "${OUTDIR}"/glne.precluster.taxonomy
@@ -69,9 +70,9 @@ mv "${TMP}"/*.precluster.pick.dist "${OUTDIR}"/glne.precluster.dist
 ###############
 
 # check that output files were created then delete temp files
-if [[ -f "${OUTDIR}"/glne.precluster.fasta && "${OUTDIR}"/glne.precluster.count_table && -f "${OUTDIR}"/glne.prec
+if [[ -f "${OUTDIR}"/glne.precluster.fasta && -f "${OUTDIR}"/glne.precluster.count_table && -f "${OUTDIR}"/glne.precluster.taxonomy && -f "${OUTDIR}"/glne.precluster.dist ]]; then
   echo PROGRESS: Cleaning up working directory.
-  #rm -r "${TMP}"/
+  rm -r "${TMP}"/
 else
   echo "Missing output file(s)"
   exit 0
