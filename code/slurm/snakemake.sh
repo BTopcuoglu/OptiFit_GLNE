b6632653642630eb94503a8ad9eaec16d61d08fe
courtneyarmour <armourc@med.umich.edu>
2023-02-24 20:24:52 -0500

    add flag to rerun incomplete files

--- code/slurm/snakemake.sh
+++ code/slurm/snakemake.sh
@@ -31,4 +31,4 @@ conda activate glne
 mkdir -p logs/slurm/
 
 # Initiating snakemake and running workflow in cluster mode
-time snakemake --profile config/slurm/ --latency-wait 90 
+time snakemake --profile config/slurm/ --latency-wait 90 --rerun-incomplete
